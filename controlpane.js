88a15eda35c07d56120552400bab94b564f9fb7a
Daye Jack <ddj231@nyu.edu>
2021-01-02 20:42:54 -0500

    add vim mode

--- controlpane.js
+++ controlpane.js
@@ -1,6 +1,8 @@
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 import { MyEditor } from './editor.js';
+import './codemirror-5.59.1/keymap/vim.js';
+
 export function ControlPane(props) {
     var _React$useState = React.useState(false),
         _React$useState2 = _slicedToArray(_React$useState, 2),
@@ -14,12 +16,8 @@ export function ControlPane(props) {
 
     var _React$useState5 = React.useState(false),
         _React$useState6 = _slicedToArray(_React$useState5, 2),
-        errorRunning = _React$useState6[0],
-        setErrorRunning = _React$useState6[1];
-
-    window.onError = function (message, source, lineno, colno, error) {
-        setErrorRunning(true);
-    };
+        vimModeChecked = _React$useState6[0],
+        setVimModeChecked = _React$useState6[1];
 
     function playClicked() {
         if (!runSelected) {
@@ -39,34 +37,54 @@ export function ControlPane(props) {
         }
     }
 
+    function handleVimModeChange() {
+        if (vimModeChecked) {
+            MyEditor.setOption("keyMap", "default");
+        } else {
+            MyEditor.setOption("keyMap", "vim");
+        }
+        setVimModeChecked(!vimModeChecked);
+        console.log('handled');
+    }
+
     return React.createElement(
-        "div",
-        { className: "controlpane" },
+        'div',
+        { className: 'controlpane' },
         React.createElement(
-            "div",
-            { onClick: playClicked, className: "mybtn " + (runSelected ? "mybtnselected" : "") },
+            'div',
+            { onClick: playClicked, className: 'mybtn ' + (runSelected ? "mybtnselected" : "") },
             React.createElement(
-                "div",
-                { className: "buttontext" },
-                "Play"
+                'div',
+                { className: 'buttontext' },
+                'Play'
             )
         ),
         React.createElement(
-            "div",
-            { onClick: stopClicked, className: "mybtn " + (!runSelected ? "mybtnselected" : "") },
+            'div',
+            { onClick: stopClicked, className: 'mybtn ' + (!runSelected ? "mybtnselected" : "") },
             React.createElement(
-                "div",
-                { className: "buttontext" },
-                "Stop"
+                'div',
+                { className: 'buttontext' },
+                'Stop'
             )
         ),
         React.createElement(
-            "a",
-            { href: "https://github.com/ddj231/Handel" },
+            'div',
+            { 'class': 'form-check' },
+            React.createElement('input', { checked: vimModeChecked, onChange: handleVimModeChange, type: 'checkbox', 'class': 'form-check-input', id: 'exampleCheck1' }),
             React.createElement(
-                "div",
-                { className: "guide" },
-                "go to language guide"
+                'label',
+                { 'class': 'vimtext', 'for': 'exampleCheck1' },
+                'vim mode'
+            )
+        ),
+        React.createElement(
+            'a',
+            { href: 'https://github.com/ddj231/Handel' },
+            React.createElement(
+                'div',
+                { className: 'guide' },
+                'go to language guide'
             )
         )
     );
--- src/controlpane.js
+++ src/controlpane.js
@@ -1,11 +1,10 @@
 import {MyEditor} from './editor.js';
+import './codemirror-5.59.1/keymap/vim.js' 
+
 export function ControlPane(props){
     let [runSelected, setRunSelected] = React.useState(false);
     let [runCount, setRunCount] = React.useState(0);
-    let [errorRunning, setErrorRunning] = React.useState(false);
-    window.onError = function(message, source, lineno, colno, error) {
-        setErrorRunning(true);
-    }
+    let [vimModeChecked, setVimModeChecked] = React.useState(false);
 
     function playClicked(){
         if(!runSelected){
@@ -25,6 +24,17 @@ export function ControlPane(props){
         }
     }
 
+    function handleVimModeChange(){
+        if(vimModeChecked){
+            MyEditor.setOption("keyMap", "default");
+        }
+        else{
+            MyEditor.setOption("keyMap", "vim");
+        }
+        setVimModeChecked(!vimModeChecked);
+        console.log('handled');
+    }
+
     return(
         <div className="controlpane">
             <div onClick={playClicked} className={`mybtn ${runSelected ? "mybtnselected": ""}`}>
@@ -32,6 +42,10 @@ export function ControlPane(props){
             </div>
             <div onClick={stopClicked} className={`mybtn ${!runSelected ? "mybtnselected": ""}`}>
                 <div className="buttontext">Stop</div>
+            </div>
+            <div class="form-check">
+                <input checked={vimModeChecked} onChange={handleVimModeChange} type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                <label class="vimtext" for="exampleCheck1">vim mode</label>
             </div>
             <a href="https://github.com/ddj231/Handel">
                 <div className="guide">

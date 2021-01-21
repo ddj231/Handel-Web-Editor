451cc81e246133c806d79f774550b0776e5da5aa
Daye Jack <ddj231@nyu.edu>
2021-01-21 14:35:44 -0500

    add reverb

--- editor.js
+++ editor.js
@@ -236,7 +236,7 @@ CodeMirror.defineSimpleMode("handel", {
   start: [{ regex: /(chunk)(\s+)([a-z$]*)/,
     token: ["keyword", null, "variable-2"] }, { regex: /(run)(\s+)([a-z$]*)/,
     token: ["keyword", null, "variable-2"] }, { regex: /(save)(\s+)([a-z$]*)/,
-    token: ["keyword", null, "variable-2"] }, { regex: /(?:play|rest|block|endblock|chunk|endchunk|run|save|start|finish|bpm|loop|sound|volume|pan)\b/,
+    token: ["keyword", null, "variable-2"] }, { regex: /(?:play|rest|block|endblock|chunk|endchunk|run|save|start|finish|bpm|loop|sound|volume|pan|reverb)\b/,
     token: "keyword" }, { regex: /synth|casio|guitar|piano|snare|kick|hihat/, token: "atom" }, { regex: /(?:for|using|with)\b/,
     token: "keyword" }]
 });
